9d57652285d74834cce22fc3a6c925ff7dadb3e1
Nikos Papadopoulos <nikpapas@gmail.com>
2017-02-23 23:24:26 +0000

    updated stb_image to latest version, added icosahedron mesh generator (1st pass)

--- libs/nmesh/icosahedron.h
+++ libs/nmesh/icosahedron.h
@@ -1,10 +1,12 @@
 #ifndef NMESH_ICOSAHEDRON_H_INCLUDED
 #define NMESH_ICOSAHEDRON_H_INCLUDED
 
+#include "structs.h"
+
 namespace nmesh {
     namespace generator {
 
-void icosahedron(object_t *obj);
+void icosahedron(NMesh::object_t *obj);
 
     } /* namespace generator */
 } /* namespace nmesh */
