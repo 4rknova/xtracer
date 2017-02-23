ccb109e42920db2813f18974b48198952b4b3e22
Nikolaos Papadopoulos <n.papadopoul@samsung.com>
2017-02-23 18:40:09 +0000

    icosahedron, first pass

---
+++ libs/nmesh/icosahedron.cc
@@ -0,0 +1,18 @@
+#include "icosahedron.h"
+
+namespace nmesh {
+    namespace generator {
+//http://blog.andreaskahler.com/2009/06/creating-icosphere-mesh-in-code.html
+void icosahedron(object_t *obj)
+{
+    if (!obj) return;
+
+/*
+    shape_t shape;
+    shape.add(mesh);
+    obj->shapes.add(shape);
+*/
+}
+
+    } /* namespace generator */
+} /* namespace nmesh */
---
+++ libs/nmesh/icosahedron.h
@@ -0,0 +1,12 @@
+#ifndef NMESH_ICOSAHEDRON_H_INCLUDED
+#define NMESH_ICOSAHEDRON_H_INCLUDED
+
+namespace nmesh {
+    namespace generator {
+
+void icosahedron(object_t *obj);
+
+    } /* namespace generator */
+} /* namespace nmesh */
+
+#endif /* NMESH_ICOSAHEDRON_H_INCLUDED */
