0a7d2e3ce0384bbe3c3a7904949bfba50c60c06b
Nikos Papadopoulos (4rknova) <nikpapas@gmail.com>
2017-05-08 00:49:21 +0100

    updated xt gui to display tiles as they render

--- src/xt_gui/gui.cpp
+++ src/xt_gui/gui.cpp
@@ -24,29 +24,24 @@
 #include "gui.h"
 
 xtracer::render::IRenderer *renderer = 0;
+xtracer::render::context_t ctx;
 
 static Shader  *postsdr;
 static Program *postprg;
-
+static GLuint   pix;
 static const char *postsdr_source =
+    "#version 130\n"
 	"uniform sampler2D tex;\n"
 	"uniform vec2 iWindowResolution;\n"
 	"uniform vec2 iRenderResolution;\n"
-	"uniform vec4 iActiveRegion;\n"
 	"uniform vec2 iTileSize;\n"
+    "out vec4 Color;\n"
 	"void main()\n"
 	"{\n"
-	"    vec3 col = texture2D(tex, gl_TexCoord[0].st).xyz;\n"
     "    vec2 uv = gl_FragCoord.xy / iWindowResolution.xy;\n"
-    "    vec2 fc = floor(uv * (iRenderResolution.xy / iTileSize.xy));\n"
-    "    col = vec3(mod(fc.x + fc.y, 2.));\n"
-    "    if (gl_FragCoord.x > iActiveRegion.x \n"
-    "     && gl_FragCoord.y > iActiveRegion.y \n"
-    "     && gl_FragCoord.x < iActiveRegion.z \n"
-    "     && gl_FragCoord.y < iActiveRegion.w){ col = vec3(0,1,0);\n"
-	"    gl_FragColor = vec4(col, 1);\n"
-    "    return;}\n"
-    "    gl_FragColor = vec4(0,0,0,1);\n"
+    "    uv.y = 1. - uv.y;\n"
+	"    vec3 cl = texture(tex, uv).xyz;\n"
+    "    Color = vec4(cl,1);\n"
 	"}\n";
 
 int window = 0;
@@ -61,59 +56,42 @@ static std::mutex mut0, mut1;
 
 static int block_begin(void *blk)
 {
-    mut0.lock();
-    mut0.unlock();
     return 0;
 }
 
 static int block_done(void *blk)
 {
-	mut1.lock();
-	dirty_areas.push((xtracer::render::tile_t*)blk);
-    xtracer::render::tile_t *t = (xtracer::render::tile_t *)blk;
-    printf("Rendered %lux%lu -> %lux%lu \n", t->x0(), t->y0(), t->x1(), t->y1());
-    postprg->set_uniform("iActiveRegion", NMath::Vector4f(t->x0(), t->y0(), t->x1(), t->y1()));
-    printf("%i %i %i %i\n",t->x0(), t->y0(), t->x1(), t->y1());
-    glutPostRedisplay();
-	mut1.unlock();
+	mut0.lock();
+	dirty_areas.push((xtracer::render::tile_t *)blk);
+	mut0.unlock();
     return 0;
 }
 
 void display(void)
 {
-    glClearColor(0, 0, 0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
-
-	/*
     // update dirty areas of the output texture
-	if (dirty_areas.size() > 0) {
-		while(dirty_count > 0) {
-			mut0.lock();
-			FrameBlock blk = dirty_areas.front();
-			dirty_areas.pop();
-			dirty_count--;
-			mut0.unlock();
+	mut0.lock();
+	while(dirty_areas.size() > 0) {
+        xtracer::render::tile_t *t = dirty_areas.front();
+		dirty_areas.pop();
 
-			float *pix = ctx.framebuf->pixels + (blk.y * ctx.framebuf->width + blk.x) * 3;
-
-			for(int i=0; i<blk.height; i++) {
-				glTexSubImage2D(GL_TEXTURE_2D, 0, blk.x, blk.y + i, blk.width, 1, GL_RGB, GL_FLOAT, pix);
-				pix += ctx.framebuf->width * 3
-			}
+		for (int y = t->y0(); y < t->y1(); ++y) {
+            for (int x = t->x0(); x < t->x1(); ++x) {
+                nimg::ColorRGBf col;
+                t->read(x, y, col);
+                float data[3] = {col.r(), col.g(), col.b()};
+                glTexSubImage2D(GL_TEXTURE_2D, 0, x, y, 1, 1, GL_RGB, GL_FLOAT, data);
+            }
 		}
-		glutPostRedisplay();
 	}
-	*/
+    mut0.unlock();
 
 	glBegin(GL_QUADS);
-	glTexCoord2f(0, 1);
-	glVertex2f(-1, -1);
-	glTexCoord2f(1, 1);
-	glVertex2f(1, -1);
-	glTexCoord2f(1, 0);
-	glVertex2f(1, 1);
-	glTexCoord2f(0, 0);
-	glVertex2f(-1, 1);
+	glTexCoord2f(0, 1);	glVertex2f(-1,-1);
+	glTexCoord2f(1, 1);	glVertex2f( 1,-1);
+	glTexCoord2f(1, 0);	glVertex2f( 1, 1);
+	glTexCoord2f(0, 0);	glVertex2f(-1, 1);
 	glEnd();
 
     glutSwapBuffers();
@@ -150,7 +128,6 @@ int main(int argc, char **argv)
 {
     std::list<std::string>modifiers;
     xtracer::render::params_t params;
-    xtracer::render::context_t ctx;
 
     std::string renderer_name, outdir, scene_path, camera;
 
@@ -203,11 +180,26 @@ int main(int argc, char **argv)
 	postprg->add_shader(postsdr);
     NMath::Vector2f ts((float)(ctx.params.tile_size), (float)(ctx.params.tile_size));
     NMath::Vector2f sz((float)(ctx.params.width    ), (float)(ctx.params.height   ));
-    postprg->set_uniform("iTileSize", ts);
-    postprg->set_uniform("iRenderResolution", sz);
 
     if(!postprg->link()) return 1;
 	bind_program(postprg);
+
+    glGenTextures(1, &pix);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, pix);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    float *data = new float[ctx.params.width*ctx.params.height*3];
+    memset(data, 0, sizeof(float) * 3 * ctx.params.width * ctx.params.height);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ctx.params.width, ctx.params.height, 0, GL_RGB, GL_FLOAT, data);
+    delete data;
+
+    postprg->set_uniform("iTileSize", ts);
+    postprg->set_uniform("iRenderResolution", sz);
+    postprg->set_uniform("tex", 0);
 
     glutMainLoop();
 
